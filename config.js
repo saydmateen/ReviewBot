5fb87ed5e0db31cd5be97905657ea52461c0f13b
Sayd Mateen <sayd.mateen@powerschool.com>
2018-04-13 14:37:45 -0700

    Added inital Jira Service file

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const config = {
   BOT_TOKEN: 'xoxb-345920159488-rUEGeiKWjj7glhr83Sqeiw1e',
-  BOT_NAME: 'slackathon'
+  BOT_NAME: 'slackathon',
+  USERNAME: "sayd.mateen@powerschool.com",
+  PASSWORD: ""
 }
 
 module.exports = config;
