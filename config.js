4e715f8ade2829eea99ea78dfafbbdde482dfea6
= <Spencer.McWilliams@Chico.Sungardps.lcl>
2018-04-13 11:21:19 -0700

    Adds config file and index.js. Adds start script to package.

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  BOT_TOKEN: 'xoxb-345920159488-rUEGeiKWjj7glhr83Sqeiw1e',
+  BOT_NAME: 'slackathon'
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,8 @@
+const SlackBot = require('slackbots');
+const Config = require('./config');
+
+// create a bot
+var bot = new SlackBot({
+    token: Config.BOT_TOKEN,
+    name: Config.BOT_NAME
+});
