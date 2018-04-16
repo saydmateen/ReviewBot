873e79e828fb5ec149409273b3ecdcdebaeb5ced
Spencer <Spencer.McWilliams@Chico.Sungardps.lcl>
2018-04-16 13:13:50 -0700

    tying slackbot to jira services

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
   CHANNEL_NAME: 'review_bot',
   PROJECT: 'BPY',
+  REQUIRED_REVIEWS: 2,
   REVIEW_COLUMN: 'Code Review',
   JIRA_ISSUE_URL: 'https://jira.powerschool.com/browse',
   PROJECT: "BPY",
--- services/test.js
+++ services/test.js
@@ -1,6 +1,7 @@
-
 const jiraService = require("./Jira");
 
-jiraService.addReview(true, "BPY-8", "Hello From Slackbot", "sayd.mateen@powerschool.com").then(response => {
-    console.log(response);
-}).catch(response => console.log(response));
+//jiraService.getTicketsUnderReview()
+jiraService.getTicketOptions("spencer.mcwilliams@powerschool.com")
+//jiraService.getAllTickets()
+.then(res => console.log(res))
+.catch(err => console.log(err));
